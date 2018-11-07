e308028bb7193ffa875803440054432222e9c53b
Jack Cattanach <jackcattanach@live.com>
2018-11-06 19:52:19 -0600

    problems with selecting data inside the login function

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express()
 const pgp = require('pg-promise')()
 const connectionString = "postgres://localhost:5432/blogsdb"
 const db = pgp(connectionString)
+const expressSession = require('express-session')
+const expressValidator = require('express-validator')
 
 users = []
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(expressSession({ secret: 'asdfdsa', saveUninitialized: false, resave: false}))
 app.engine('mustache',mustacheExpress())
 app.set('views','./views')
 app.set('view engine','mustache')
@@ -23,9 +25,22 @@ app.get('/create-post', function(req,res){
   res.render('create-post')
 })
 
+app.post('/create-post', function(req,res){
+  let postTitle = req.body.postTitle
+  let postBody = req.body.postBody
+
+  db.none('INSERT INTO posts(title, body, userId) VALUES ($1,$2,$3)',[postTitle, postBody, 5]).then(function(){
+    res.redirect('/view-posts')
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+})
+
 app.get('/register', function(req,res){
   res.render('register')
 })
+
 app.post('/register', function(req,res){
   let registerUsername = req.body.username
   let registerPassword = req.body.password
@@ -46,15 +61,12 @@ app.post('/login', function(req,res){
   let loginUsername = req.body.username
   let loginPassword = req.body.password
 
-  for(let index = 0; index < users.length; index++){
-    if (loginUsername == users[index].username && loginPassword == users[index].password){
-      console.log("Login successful")
-      res.redirect('/view-posts')
-    } else {
-      console.log('Username or password is incorrect')
-      res.redirect('/login')
-    }
-  }
+
+  db.one('SELECT userid, username FROM users').then(function(result){
+    res.send(result)
+  })
+  req.session.user = loginUsername
+
 })
 
 app.get('/', function(req,res){
